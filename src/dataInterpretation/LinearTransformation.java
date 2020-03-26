c9412ed0a0d7cf31e402e671be463994b1e6e81c
SpikeYr <emil.f.anderson@gmail.com>
2020-03-26 09:43:28 +0100

    Namnförändring av metod

--- src/dataInterpretation/LinearTransformation.java
+++ src/dataInterpretation/LinearTransformation.java
@@ -1,7 +1,7 @@
 package dataInterpretation;
 
 public abstract class LinearTransformation {
-	private Double Transform(Double DPValue, Double DPMin, Double DPMax, Double ParamMin, Double ParamMax) {
+	protected Double ValueTransform(Double DPValue, Double DPMin, Double DPMax, Double ParamMin, Double ParamMax) {
 		Double DPOut = 0.0;
 
 		DPOut = ((DPValue - DPMin) / (DPMax - DPMin)) * (ParamMax - ParamMin) + (ParamMin);
